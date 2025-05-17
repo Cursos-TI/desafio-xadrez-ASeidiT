3540121cbacd814ec05dbdc70f9b7e1a0e981c1f
ANDRE SEIDI TSUKIDE <eco.cmb@gmail.com>
2025-05-17 00:48:02 +0000

    Tema 3 - Desafio Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,14 +5,17 @@ int main(){
     // Usaremos a letra 't' para a Torre
     // Usaremos a letra 'b' para o Bispo
     // Usaremos a letra 'r' para a Rainha
+    // Usaremos a letra 'c' para o Cavalo
+
     int b = 0, r = 0;
     
     //Mover a torre 5 casas para a direita
     for (int t = 0; t < 5; t++)
     {
         printf("Torre - ");
-        printf("Direita\n"); //Imprime a direção do movimento
+        printf("Direita\n"); //Imprime a direção do movimento        
     }
+    printf("\n");
 
     //Mover o Bispo 5 casas na diagonal para cima e à direita
     do
@@ -21,16 +24,29 @@ int main(){
         printf("Cima, Direita\n");
         b++;
     } while (b < 5);
-    
+    printf("\n");
 
     //Mover a Rainha 8 casas para a esquerda
     while (r < 8)
     {
         printf("Rainha - ");
         printf("Esquerda\n");
-        r++;
+        r++; 
     }
+        printf("\n");
     
+    for (int c = 0; c < 1; c++)
+    {
+        while (c < 2)
+        {
+            printf("Cavalo ");
+            printf("Cima\n");
+            c++;
+        }
+        printf("Cavalo ");
+        printf("Direita\n");
+        printf("\n");
+    }
 
     return 0;
 }
